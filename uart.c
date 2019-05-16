0c6936041fd35c7d79b2d08d7ac48b2dca3d6509
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2019-05-16 22:23:02 +0300

    improve uart

--- uart.c
+++ uart.c
@@ -5,14 +5,14 @@
 #define F_CPU 16000000UL
 #endif
 
-#ifndef BAUD
-#define BAUD 9600
-#endif
-#include <util/setbaud.h>
 
 /* http://www.cs.mun.ca/~rod/Winter2007/4723/notes/serial/serial.html */
 
-void uart_init(void) {
+void uart_9600(void) {
+#undef BAUD
+#define BAUD 9600
+#include <util/setbaud.h>
+
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
     
@@ -22,19 +22,54 @@ void uart_init(void) {
     UCSR0A &= ~(_BV(U2X0));
 #endif
 
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */ 
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */    
+    uart_shared();
 }
 
-void uart_putchar(char c, FILE *stream) {
-    if (c == '\n') {
-        uart_putchar('\r', stream);
-    }
+void uart_4800(void) {
+#undef BAUD
+#define BAUD 4800
+#include <util/setbaud.h>
+
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
+
+    uart_shared();
+}
+
+void uart_shared(void) {
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00) | _BV(UCSZ20); /* 9-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
+}
+
+void uart_send(unsigned int data) {
     loop_until_bit_is_set(UCSR0A, UDRE0);
-    UDR0 = c;
+    /* Copy 9th bit to TXB8 */
+    UCSR0B &= ~(1<<TXB8);
+    if ( data & 0x0100 ) {
+        UCSR0B |= (1<<TXB8);
+    }
+    UDR0 = data;
 }
 
-char uart_getchar(FILE *stream) {
+unsigned int uart_receive() {
+    unsigned char status, resh, resl;
+    /* Wait for data to be received */
     loop_until_bit_is_set(UCSR0A, RXC0);
-    return UDR0;
+    /* Get status and 9th bit, then data from buffer */
+    status = UCSR0A;
+    resh = UCSR0B;
+    resl = UDR0;
+    /* If error, return -1 */
+    if ( status & (1<<FE0)|(1<<DOR0)|(1<<UPE0) ) {
+        return -1;
+    }
+    /* Filter the 9th bit, then return */
+    resh = (resh >> 1) & 0x01;
+    return ((resh << 8) | resl);
 }
--- uart.h
+++ uart.h
@@ -1,9 +1,4 @@
-void uart_putchar(char c, FILE *stream);
-char uart_getchar(FILE *stream);
-
-void uart_init(void);
-
-/* http://www.ermicro.com/blog/?p=325 */
-
-FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
-FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+void uart_9600(void);
+void uart_4800(void);
+void uart_send(unsigned int data);
+unsigned int uart_receive();
