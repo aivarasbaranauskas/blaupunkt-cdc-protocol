bbbc3f22f46c5a1992ff1464742fb78f99cc6e7a
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2020-05-27 21:12:03 +0300

    return main to original code

--- main.c
+++ main.c
@@ -20,14 +20,44 @@
 #include "main.h"
 #include "uart.h"
 
+
+#define MAX_MESSAGE_LENGTH 16
+
+#define STATUS_UNINITIALISED 0
+#define STATUS_INITIALISATION_STARTED 1
+#define STATUS_INITIALISED 2
+
+#define MESSAGE_START 0x180
+#define MESSAGE_END 0x14F
+
 int main(void) {
-    unsigned int r;
+    uint8_t status = STATUS_UNINITIALISED;
+    unsigned int msg[MAX_MESSAGE_LENGTH];
+    unsigned int msg_len;
+    unsigned int msg_received;
+
+    // start at 4800
     uart_4800();
 
-    for (;;) {
-        r = uart_receive();
-        _delay_ms(10);
-        uart_send(r);
+    // main loop
+    while (1) {
+        // receive message
+        msg_received = 0;
+        msg_len = 0;
+        while (!msg_received) {
+            msg[msg_len] = uart_receive();
+            if (msg[msg_len] == MESSAGE_END) {
+                msg_received = 1;
+            } else {
+                // echo back everything except MESSAGE_END 
+                uart_send(msg[msg_len]);
+            }
+            msg_len++;
+        }
+        
+        // interpret the message
+
+        
     }
 
     return 0;
